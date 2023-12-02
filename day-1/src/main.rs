ccc26a5978a077e18ea7312cbca46b7200ae9117
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-12-01 20:25:45 -0700

    Complete Day 1
    Optimized my slow solution with dehan-jl's solution using several maps.
    I got the right answer on my own but it was taking around 25 seconds to
    parse everything which I know is way longer than it should be taking for
    this.
    
    Need to make more progress commits starting tomorrow so I can actually
    look back at my own solutions before I optimize it with solutions from
    others.

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs;
+
+fn read_input_file(part: bool) -> u32 {
+    fs::read_to_string("./part-one-input.txt")
+        .unwrap()
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            if part {
+                line.to_string()
+                    .replace("one", "one1one")
+                    .replace("two", "two2two")
+                    .replace("three", "three3three")
+                    .replace("four", "four4four")
+                    .replace("five", "five5five")
+                    .replace("six", "six6six")
+                    .replace("seven", "seven7seven")
+                    .replace("eight", "eight8eight")
+                    .replace("nine", "nine9nine")
+            } else {
+                line.to_string()
+            }
+        })
+        .map(|line| {
+            line.chars()
+                .filter_map(|char| char.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .map(|vec| 10 * vec.first().unwrap() + vec.last().unwrap())
+        .sum()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Total sum for part one: {}", read_input_file(false));
+    println!("Total sum for part two: {}", read_input_file(true));
 }
