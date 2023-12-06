e18c56e090715393ff66c73b6b9cc62fddef3852
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-12-06 11:10:07 -0700

    Complete part 2 & day 4

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,13 +1,18 @@
 use std::fs;
 
-fn part_one() -> u32 {
+fn part_one_and_two() -> (u32, u32) {
     let mut sum = 0;
+    let mut total_games: Vec<u32> = vec![0; 211];
+
     let file = fs::read_to_string("./input.txt")
         .expect("File should be found at the path relative to src dir");
 
     let lines = file.lines().collect::<Vec<&str>>();
 
+    let mut game_idx: usize = 0;
     for line in lines {
+        total_games[game_idx] += 1;
+
         let idx_colon: usize = line.find(":").unwrap();
         let idx_bar: usize = line.find("|").unwrap();
 
@@ -31,12 +36,21 @@ fn part_one() -> u32 {
 
         if num_winning_in_numbers > 0 {
             sum += 2 << (num_winning_in_numbers - 1);
+
+            for game in game_idx + 1..num_winning_in_numbers + game_idx + 1 {
+                total_games[game] += 1 * total_games[game_idx];
+            }
         }
+
+        game_idx += 1;
     }
 
-    sum / 2
+    (sum / 2, total_games.iter().sum())
 }
 
 fn main() {
-    println!("Total for part one: {}", part_one());
+    let (part_one, part_two) = part_one_and_two();
+
+    println!("Total for part one: {}", part_one);
+    println!("Total for part two: {}", part_two);
 }
