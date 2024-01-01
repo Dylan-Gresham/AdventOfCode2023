ce482090fb5daeda8ced34117b02240767158210
Dylan Gresham <98201122+Dylan-Gresham@users.noreply.github.com>
2023-12-31 17:52:03 -0800

    Finish part 2 & day 6

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -33,6 +33,37 @@ fn part_one() -> u32 {
         .fold(1, |cumprod, x| cumprod * x)
 }
 
+fn part_two() -> u32 {
+    let file = fs::read_to_string("./input.txt").expect("File should exist");
+    let lines = file.lines().collect::<Vec<&str>>();
+
+    let times_raw: &str = lines[0].split(":").skip(1).collect::<Vec<&str>>()[0];
+    let time: u64 = times_raw
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+
+    let distances_raw: &str = lines[1].split(":").skip(1).collect::<Vec<&str>>()[0];
+    let distance: u64 = distances_raw
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+
+    let mut num_ways_to_win: u32 = 0;
+    for time_holding in 0..=time {
+        if time_holding * (time - time_holding) > distance {
+            num_ways_to_win += 1;
+        }
+    }
+
+    num_ways_to_win
+}
+
 fn main() {
-    println!("Total for part_one: {}", part_one());
+    println!("Total for part one: {}", part_one());
+    println!("Total for part two: {}", part_two());
 }
